47c0380e06908dee4a190a2f52bd35a5fc1f9be4
ygb <ygb17630907842@163.com>
2019-11-06 13:41:36 +0800

    第一次修改

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -17,6 +17,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("---------------------------");
+        System.out.println("---------------------------");
     }
 
     @Bean
